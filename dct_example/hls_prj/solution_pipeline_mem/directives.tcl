4997d2406dfe2d0d1eee916d6ef924ad44b734ef
Nick Butts <buttsnicholasl@johndeere.com>
2022-01-13 06:24:21 -0600

    Checked in all of the solutions.

---
+++ dct_example/hls_prj/solution_pipeline_mem/directives.tcl
@@ -0,0 +1,9 @@
+############################################################
+## This file is generated automatically by Vitis HLS.
+## Please DO NOT edit it.
+## Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+############################################################
+set_directive_top -name dct "dct"
+set_directive_pipeline "dct"
+set_directive_bind_storage -type ram_2p "dct" buf_2d_in
+set_directive_bind_storage -type ram_2p "dct" buf_2d_out
