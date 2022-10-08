4fe4405ca85acfec21a8967958a45fb512995326
Nick Butts <nlbutts@ieee.org>
2022-10-08 09:00:10 -0500

    Have the accel compiling for the ZCU104 board. Now trying to get the sd card to build.

---
+++ board/zcu106.srcs/sources_1/imports/hdl/MPSoC_ext_platform_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2022.1 (lin64) Build 3526262 Mon Apr 18 15:47:01 MDT 2022
+//Date        : Sat Oct  8 08:07:53 2022
+//Host        : Dell-G15-5515 running 64-bit Ubuntu 20.04.5 LTS
+//Command     : generate_target MPSoC_ext_platform_wrapper.bd
+//Design      : MPSoC_ext_platform_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module MPSoC_ext_platform_wrapper
+   ();
+
+
+  MPSoC_ext_platform MPSoC_ext_platform_i
+       ();
+endmodule
